463d40b58591f93146e46a01a628bad99d275b9f
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-08-06 20:23:12 +0300

    fix: complete integration with model output data

--- predict_me/static/assets/js/custom/members_app/dashboard_apexcharts.js
+++ predict_me/static/assets/js/custom/members_app/dashboard_apexcharts.js
@@ -43,86 +43,136 @@ var KTApexChartsDemo = function () {
     // Private functions
 
     var _trainingTestingChart = async function () {
+        const trainingAndTestingFullWrapper = document.querySelector('#training-and-testing-full-wrapper');
+        const wrapperLoaderIcon = trainingAndTestingFullWrapper.querySelector('div.dashboard-loader');
+        const wrapperCardElement = trainingAndTestingFullWrapper.querySelector('div.card-custom');
+        const wrapper = document.querySelector("#trainAndTestWrapper");
+        const noWrapper = document.querySelector("#noTrainingAndTestingWrapper");
+        const body = document.querySelector('#noTrainingAndTestingBody');
         const trainTestRequest = await fetchDataToSessionDashboard('training_testing');
         const trainingData = trainTestRequest['data']['training'];
         const testingData = trainTestRequest['data']['testing'];
         const apexChart = "#session_dashboard_training_testing_chart";
-        const options = {
-            dataLabels: {
-                enabled: true,
-                formatter: function (val) {
-                    const newVal = Math.round(val);
-                    return newVal.toString() + "%";
-                },
-
-            },
-            series: [trainingData, testingData],
-            labels: ["Train", "Test"],
-            chart: {
-                width: 380,
-                type: 'donut',
-            },
-            responsive: [{
-                breakpoint: 480,
-                options: {
-                    chart: {
-                        width: 200
+        // check if the training and testing data not null
+        if ((trainingData !== false) && (testingData !== false)) {
+            // remove the d-none class from the wrapper
+            wrapper.classList.remove("d-none");
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            const options = {
+                dataLabels: {
+                    enabled: true,
+                    formatter: function (val) {
+                        const newVal = Math.round(val);
+                        return newVal.toString() + "%";
                     },
-                    legend: {
-                        position: 'bottom'
-                    }
-                }
-            }],
-            colors: [primary, success, warning, danger, info]
-        };
 
-        const chart = new ApexCharts(document.querySelector(apexChart), options);
-        chart.render();
+                },
+                series: [trainingData, testingData],
+                labels: ["Train", "Test"],
+                chart: {
+                    width: 380,
+                    type: 'donut',
+                },
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }],
+                colors: [primary, success, warning, danger, info]
+            };
+
+            const chart = new ApexCharts(document.querySelector(apexChart), options);
+            chart.render();
+        } else {
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            // hide the wrapper first
+            wrapper.classList.add('d-none');
+            // remove the d-none class from the wrapper
+            noWrapper.classList.remove("d-none");
+            body.classList.add("h5");
+            body.innerHTML = trainTestRequest['data']['donation_columns'];
+        }
+
     }
 
     var _sessionDashboardImportantFeaturesChart = async function () {
+        const importantFeaturesFullWrapper = document.querySelector("#important-features-full-wrapper");
+        const wrapperLoaderIcon = importantFeaturesFullWrapper.querySelector('div.dashboard-loader');
+        const wrapperCardElement = importantFeaturesFullWrapper.querySelector('div.card-custom');
+        const wrapper = document.querySelector("#importantFeaturesWrapper");
+        const noWrapper = document.querySelector("#noImportantFeaturesWrapper");
+        const body = document.querySelector("#noImportantFeaturesBody");
         const fetchDataRequest = await fetchDataToSessionDashboard('important_features');
         const featureImportanceLabels = fetchDataRequest['data']['labels'];
         const featureImportanceValues = fetchDataRequest['data']['values'];
         const apexChart = "#session_dashboard_important_features_chart";
-        // console.log(fetchDataRequest['data']);
-        const options = {
-            
-            series: [{
-                data: featureImportanceValues.reverse()
-            }],
-            chart: {
-                type: 'bar',
-                height: 350,
-               /*  events: {
-                    beforeMount: function (chartContext, options) {
-                        console.log('beforeMount');
-                    },
-                    mounted: function (chartContext, options) {
-                        console.log('mounted');
-                    },
-                }, */
-            },
-            plotOptions: {
-                bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                }
-            },
-            dataLabels: {
-                enabled: false
-            },
-            legend: {
-                show: true,
-                position: 'bottom'
-            },
-            xaxis: {
-                categories: featureImportanceLabels.reverse(),
-            }
-        };
+        // check if the values are array contain values
+        if (Array.isArray(featureImportanceValues) === true) {
 
-        const chart = new ApexCharts(document.querySelector(apexChart), options);
-        chart.render();
+            // remove the d-none class from the wrapper
+            wrapper.classList.remove("d-none");
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            const options = {
+
+                series: [{
+                    data: featureImportanceValues.reverse()
+                }],
+                chart: {
+                    type: 'bar',
+                    height: 350,
+                    /*  events: {
+                         beforeMount: function (chartContext, options) {
+                             console.log('beforeMount');
+                         },
+                         mounted: function (chartContext, options) {
+                             console.log('mounted');
+                         },
+                     }, */
+                },
+                plotOptions: {
+                    bar: {
+                        borderRadius: 4,
+                        horizontal: true,
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                legend: {
+                    show: true,
+                    position: 'bottom'
+                },
+                xaxis: {
+                    categories: featureImportanceLabels.reverse(),
+                }
+            };
+
+            const chart = new ApexCharts(document.querySelector(apexChart), options);
+            chart.render();
+        } else {
+            // colors array
+            const warningColors = ['bg-diagonal', 'bg-diagonal-light-warning'];
+            // change the color of the card
+            wrapperCardElement.classList.add(...warningColors);
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            // hide the wrapper first
+            wrapper.classList.add('d-none');
+            // remove the d-none class from the wrapper
+            noWrapper.classList.remove("d-none");
+            body.classList.add("h5");
+            body.innerHTML = featureImportanceValues;
+        }
+
     }
 
     return {
--- predict_me/static/assets/js/custom/members_app/dashboard_jqvmap.js
+++ predict_me/static/assets/js/custom/members_app/dashboard_jqvmap.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Class definition
-var KTjQVMapDemo = function() {
+var KTjQVMapDemo = function () {
 
     var sample_data = {
         "af": "16.63",
@@ -191,60 +191,89 @@ var KTjQVMapDemo = function() {
 
     // Private functions
 
-    var setupMap = function(name) {
-        var data = {
-            map: 'world_en',
-            backgroundColor: null,
-            color: '#ffffff',
-            hoverOpacity: 0.7,
-            selectedColor: '#666666',
-            enableZoom: true,
-            showTooltip: true,
-            values: sample_data,
-            scaleColors: ['#C8EEFF', '#006491'],
-            normalizeFunction: 'polynomial',
-            onRegionOver: function(event, code) {
-                //sample to interact with map
-                if (code == 'ca') {
-                    event.preventDefault();
+    var setupMap = async function (name) {
+        // colors array
+        const warningColors = ['bg-diagonal', 'bg-diagonal-light-warning'];
+        const mapFullWrapper = document.querySelector('#map-full-wrapper');
+        const wrapperLoaderIcon = mapFullWrapper.querySelector('div.dashboard-loader');
+        const wrapperCardElement = mapFullWrapper.querySelector('div.card-custom');
+        const wrapper = document.querySelector("#map-wrapper");
+        const noWrapper = document.querySelector('#no-map-wrapper');
+        const body = document.querySelector("#noMapBody");
+        const fetchDataRequest = await fetchDataToSessionDashboard('geo_location_fields');
+        const geoLocationData = fetchDataRequest['data']['geo_location_data'];
+
+        // check if there geo-location data
+        if (geoLocationData !== null) {
+            // remove the d-none class from the wrapper
+            wrapper.classList.remove("d-none");
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            var data = {
+                map: 'world_en',
+                backgroundColor: null,
+                color: '#ffffff',
+                hoverOpacity: 0.7,
+                selectedColor: '#666666',
+                enableZoom: true,
+                showTooltip: true,
+                values: sample_data,
+                scaleColors: ['#C8EEFF', '#006491'],
+                normalizeFunction: 'polynomial',
+                onRegionOver: function (event, code) {
+                    //sample to interact with map
+                    if (code == 'ca') {
+                        event.preventDefault();
+                    }
+                },
+                onRegionClick: function (element, code, region) {
+                    //sample to interact with map
+                    var message = 'You clicked "' + region + '" which has the code: ' + code.toUpperCase();
+                    alert(message);
                 }
-            },
-            onRegionClick: function(element, code, region) {
-                //sample to interact with map
-                var message = 'You clicked "' + region + '" which has the code: ' + code.toUpperCase();
-                alert(message);
-            }
-        };
+            };
 
-        data.map = name + '_en';
+            data.map = name + '_en';
 
-        var map = jQuery('#kt_jqvmap_' + name);
+            var map = jQuery('#kt_jqvmap_' + name);
 
-        map.width(map.parent().width());
-        map.vectorMap(data);
+            map.width(map.parent().width());
+            map.vectorMap(data);
+        } else {
+            // change the color of the card
+            wrapperCardElement.classList.add(...warningColors);
+            // hide the loader icon
+            wrapperLoaderIcon.classList.add('d-none');
+            // hide the wrapper first
+            wrapper.classList.add('d-none');
+            // remove the d-none class from the wrapper
+            noWrapper.classList.remove("d-none");
+            body.classList.add("h5");
+            body.innerHTML = "No Geo location Data";
+        }
     }
 
-    var setupMaps = function() {
+    var setupMaps = function () {
         setupMap("world");
-        setupMap("usa");
-        setupMap("europe");
-        setupMap("russia");
-        setupMap("germany");
+        // setupMap("usa");
+        // setupMap("europe");
+        // setupMap("russia");
+        // setupMap("germany");
     }
 
     return {
         // public functions
-        init: function() {
+        init: function () {
             // default charts
             setupMaps();
 
-            KTUtil.addResizeHandler(function() {
+            KTUtil.addResizeHandler(function () {
                 setupMaps();
             });
         }
     };
 }();
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     KTjQVMapDemo.init();
 });
