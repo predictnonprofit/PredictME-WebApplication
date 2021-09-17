c21cb67112d6a7f315d968ed3db587b77e38cf5a
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-17 22:58:06 +0300

    feat: create MemberMessagesForm

--- messages_app/forms.py
+++ messages_app/forms.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-#
+from django.forms import ModelForm
+from .models import MemberMessages
+
+
+class MemberMessagesForm(ModelForm):
+    class Meta:
+        model = MemberMessages
+        fields = ('sender', 'subject', "other_subject", "attachment", 'message')
+
