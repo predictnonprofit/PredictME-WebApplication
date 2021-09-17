151c4d7bb21d1af926df590500441cbc1b5c61a4
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-17 22:54:29 +0300

    feat: create MemberMessages model

--- messages_app/models.py
+++ messages_app/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from users.models import Member
+from predict_me.constants.vars import SUBJECTS_TYPES
 
-# Create your models here.
 
+class MemberMessages(models.Model):
+    sender = models.ForeignKey(to=Member, on_delete=models.CASCADE, related_name='messages_sent')
+    reply = models.ForeignKey(to='self', on_delete=models.CASCADE, null=True, blank=True, related_name='message_replies')
+    subject = models.CharField(choices=SUBJECTS_TYPES, max_length=100, null=True, blank=True)
+    other_subject = models.CharField(max_length=100, null=True, blank=True)
+    attachment = models.FileField(upload_to='message_attachments', null=True, blank=True)
+    message = models.TextField(null=False)
+    send_date = models.DateTimeField(auto_now_add=True)
+    is_seen = models.BooleanField(default=False, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False, null=True, blank=True)
 
-# class MemberMessages(models.Model):
-#     class Meta:
-#         db_table = 'members_messages'
-#
-#     def __str__(self):
-#         return f"Message from {self.sender} to {self.receiver}, it's title {self.subject}"
-#
-#     sender = models.ForeignKey(to=Member, on_delete=models.CASCADE, related_name='messages_send')
-#     receiver = models.ForeignKey(to=Member, on_delete=models.CASCADE, related_name="messages_received")
-#     subject = models.CharField(max_length=250, null=True, blank=True)
-#     file_attachment = models.FileField(upload_to='message_attachments', null=True, blank=True)
-#     message = models.TextField()
-#     send_date = models.DateTimeField(auto_now_add=True)
-#     is_seen = models.BooleanField(default=False, null=True, blank=True)
-#     is_deleted = models.BooleanField(default=False, null=True, blank=True)
+    class Meta:
+        db_table = 'member_messages'
 
+    def __str__(self):
+        return f"Message from {self.sender}, it's title {self.subject}"
