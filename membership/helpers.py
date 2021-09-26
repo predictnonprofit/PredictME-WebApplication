2ff95f11e603b03db4b1900a7cca96858a1fe6f0
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-26 17:29:51 +0300

    fix: fix membership in subscription object

--- membership/helpers.py
+++ membership/helpers.py
@@ -1,8 +1,10 @@
 from termcolor import cprint
 from predict_me.helpers import check_internet_access
+from predict_me.my_logger import log_exception
 from users.models import Member
 import stripe
 import os
+import traceback
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
@@ -12,24 +14,28 @@ def get_subscription_and_membership(member: Member):
     This function will return the subscription and membership objects for any member,
     and will handler administrator account issue because admin has no membership and subscription objects
     """
-    member_data = dict()
-    member_data["is_superuser"] = member.is_superuser
-    subscription_obj = member.member_subscription.filter().first()
-    if subscription_obj is not None:
-        # this case the user not administrator account
-        data_handler_obj = member.member_data_file.filter().first()
-        member_data['subscription_obj'] = subscription_obj
-        member_data["membership_obj"] = subscription_obj.stripe_plan_id
-        member_data['data_handler_obj'] = data_handler_obj
-        member_data['data_handler_session'] = data_handler_obj.data_sessions_set.filter().all()
-    else:
-        # this case the user is administrator account
-        member_data['subscription_obj'] = None
-        member_data["membership_obj"] = None
-        member_data['data_handler_obj'] = None
-        member_data['data_handler_session'] = None
+    try:
+        member_data = dict()
+        member_data["is_superuser"] = member.is_superuser
+        subscription_obj = member.subscription.filter().first()
+        if subscription_obj is not None:
+            # this case the user not administrator account
+            data_handler_obj = member.member_data_file.filter().first()
+            member_data['subscription_obj'] = subscription_obj
+            member_data["membership_obj"] = subscription_obj.membership
+            member_data['data_handler_obj'] = data_handler_obj
+            member_data['data_handler_session'] = data_handler_obj.data_sessions_set.filter().all()
+        else:
+            # this case the user is administrator account
+            member_data['subscription_obj'] = None
+            member_data["membership_obj"] = None
+            member_data['data_handler_obj'] = None
+            member_data['data_handler_session'] = None
 
-    return member_data
+        return member_data
+    except Exception as ex:
+        cprint(traceback.format_exc(), 'red')
+        log_exception(traceback.format_exc())
 
 
 def get_member_total_charges(member: Member):
@@ -37,7 +43,7 @@ def get_member_total_charges(member: Member):
     This function will calculate the total charges for the member from stripe
     """
     all_charge_amount = []
-    subscription_obj = member.member_subscription.filter().first()
+    subscription_obj = member.subscription.filter().first()
     if subscription_obj is not None:
         check_connection = check_internet_access()  # just for developments
         if check_connection is True:
@@ -48,6 +54,3 @@ def get_member_total_charges(member: Member):
             return {'all_charges_amount': sum(all_charge_amount), "charges_sum": len(all_charge_amount)}
         else:
             return {}
-
-
-
