f34a6095ae79205a026b5e396f4c7dbaf79a8e31
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-26 17:31:00 +0300

    feat: upgrade membership and update credit card urls now working

--- membership/urls.py
+++ membership/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("update-stripe", UpdateStripeView.as_view(), name="membership-account-update-stripe"),
-    path("upgrade-membership", UpgradeMembershipView.as_view(), name='membership-upgrade-membership'),
+    path("update-credit-card", UpdateStripeCreditCardView.as_view(), name="membership-account-update-credit-card"),
+    # check where this used in template files don't delete the class view
+    # path("upgrade-membership", UpgradeMembershipView.as_view(), name='membership-upgrade-membership'),
     path("charge-extra-records", ChargeExtraRecordView.as_view(), name='membership-charge-extra-records'),
     path("change-stripe-card", ChangeStripeCardView.as_view(), name='membership-change-stripe-card'),
     path("api/", include([
-        path("upgrade-membership-api", UpgradeMembershipAPIView.as_view(), name='api-upgrade-membership')
+        path("upgrade-membership-api-only", UpgradeMembershipAPIView.as_view(), name='api-upgrade-membership-only')
     ]), name='membership-api-urls')
 ]
