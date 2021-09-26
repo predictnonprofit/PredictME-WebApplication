c3f78246ecb8c885a40e27a76c8990bd42b7fe42
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-26 17:30:07 +0300

    fix: refactor subscription model

--- membership/models.py
+++ membership/models.py
@@ -29,9 +29,6 @@ SUB_RANGE = (
 
 
 class Membership(models.Model):
-    # class Meta:
-    #     db_table = "membership"
-
     slug = models.SlugField(null=True, blank=True, db_index=True)
     membership_type = models.CharField(choices=MEMBERSHIP_LABELS, max_length=30, null=True, blank=True)
     monthly_fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -44,6 +41,12 @@ class Membership(models.Model):
     parent = models.CharField(choices=PARENTS_MEMBERSHIP_LABELS, max_length=30, null=True, blank=True)
     range_label = models.CharField(max_length=20, choices=SUB_RANGE, null=True, blank=True)
 
+    class Meta:
+        db_table = "membership"
+
+    def __str__(self):
+        return f"Membership {self.slug}"
+
     @property
     def get_fields_as_list(self):
         fields = self._meta.fields
@@ -51,31 +54,30 @@ class Membership(models.Model):
         for fid in fields:
             fields_list.append(fid.name)
         return fields_list
-
-    def __str__(self):
-        return self.membership_type
 
 
 class Subscription(models.Model):
     objects = SubscribeManager()
-    member_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="member_subscription",
-                                  null=True, blank=True, related_query_name="member_subscription")
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="subscription", null=True,
+                               blank=True, related_query_name="member_subscription")
     stripe_customer_id = models.CharField(max_length=100, null=True, blank=True)
+    stripe_card_token = models.CharField(max_length=200, null=True, blank=True)
     stripe_subscription_id = models.CharField(max_length=60, null=True, blank=True)
-    stripe_plan_id = models.ForeignKey(Membership, on_delete=models.CASCADE, blank=True, null=True)
+    # stripe_plan_id = models.ForeignKey(Membership, on_delete=models.CASCADE, blank=True, null=True)
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE, related_name="membership", blank=True,
+                                   null=True)
     subscription_status = models.BooleanField(null=True, blank=True)
     subscription_period_start = models.DateTimeField(null=True, blank=True)
     subscription_period_end = models.DateTimeField(null=True, blank=True)
     card_expire = models.DateTimeField(null=True, blank=True)
-    sub_range = models.CharField(max_length=20, choices=SUB_RANGE, null=True, blank=True)
     stripe_card_id = models.CharField(max_length=50, null=True, blank=True)
     last_invoice_id = models.CharField(max_length=200, null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.member_id} Subscription Object"
-
     class Meta:
         db_table = "subscriptions"
+
+    def __str__(self):
+        return f"Subscription Object for {self.member}"
 
     @property
     def get_fields_as_list(self):
@@ -84,15 +86,3 @@ class Subscription(models.Model):
         for fid in fields:
             fields_list.append(fid.name)
         return fields_list
-
-
-class Charges(models.Model):
-    class Meta:
-        db_table = "charges"
-
-    member = models.ForeignKey(to=Member, on_delete=models.CASCADE, related_name='charges', db_index=True)
-    charge_token = models.CharField(max_length=100, unique=True)
-    amount = models.BigIntegerField(null=True, blank=True)
-    # invoice_token = models.CharField(max_length=100, null=True, blank=True)
-    status = models.CharField(max_length=20, null=True, blank=True)
-    charge_date = models.DateTimeField(auto_now_add=True)
