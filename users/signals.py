f85d8b67d8abc9fbe71ca318900a0e00e21331e8
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-09-26 17:36:09 +0300

    fix: refactor the code with new subscription

--- users/signals.py
+++ users/signals.py
@@ -1,20 +1,22 @@
-from django.db.models.signals import post_save
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseNotFound
-from django.contrib.auth import get_user_model
-from membership.models import (Subscription)
-from data_handler.models import (DataFile, DataHandlerSession)
+import traceback
+
 import stripe
-import os
-from termcolor import cprint
 from django.conf import settings
-from prettyprinter import pprint
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.db import transaction
+from termcolor import cprint
+
+from data_handler.models import (DataFile)
+from membership.models import (Subscription)
+from predict_me.my_logger import log_exception
+
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-def create_usermembership_memberdatafile(sender, instance, created, **kwargs):
+def create_subscription_datafile_object_for_member(sender, instance, created, **kwargs):
     """
-    this function will run after new member register, to create usermembership
+    this function will run after new member register, to create subscription for members
     object to the new member,
     Arguments:
         sender {[type]} -- [description]
@@ -24,40 +26,39 @@ def create_usermembership_memberdatafile(sender, instance, created, **kwargs):
     try:
         # print(sender, instance, created, kwargs)
         if created:
-            subscription_obj, created = Subscription.objects.get_or_create(member_id=instance)
-            # cprint(subscription_obj, "yellow")
-            # check if the new member has stripe id
-            if subscription_obj.stripe_plan_id is None or subscription_obj.stripe_plan_id == "":
-                # member = instance
-                new_member_id = stripe.Customer.create(email=instance.email, name=instance.full_name)
-                # subscription = stripe.Subscription.create(
-                #     customer=new_member_id['id'],
-                #     items=[
-                #         {"price": "price_1HW4LjCXFX3jJJ8K8pzAc8uu"},
-                #     ],
-                # )
-                # pprint(new_member_id)
-                subscription_obj.stripe_customer_id = new_member_id['id']
-                instance.stripe_customer_id = new_member_id['id']
-                instance.save()
-                # create data file object for the new member
-                data_file = DataFile()
-                data_file.member = instance
-                data_file.file_upload_procedure = None
-                data_file.data_file_path = None
-                data_file.save()
-                # subscription_obj.data_file_obj = data_file
-                subscription_obj.save()
+            with transaction.atomic():
+                stripe_metadata = {}
+                stripe_description = ""
+                # check if it is in debug mode
+                if settings.DEBUG is True:
+                    # metadata["user_type"] = "development"
+                    stripe_metadata.update({"user_type": "development"})
+                    stripe_description = f"{instance.full_name} :-> Test Customer"
+                else:
+                    stripe_metadata.update({"user_type": "production"})
+                    stripe_description = f"{instance.full_name} :-> Production Customer"
 
-            # data_file_obj, created = DataFile.objects.get_or_create(member=user_membership_obj)
-            # data_file_obj.save()
+                subscription_obj, created = Subscription.objects.get_or_create(member=instance)
+                # check if the new member has stripe id
+                if (subscription_obj.membership is None) or (subscription_obj.membership == ""):
+                    new_member_id = stripe.Customer.create(email=instance.email, name=instance.full_name,
+                                                           description=stripe_description, metadata=stripe_metadata)
+                    subscription_obj.stripe_customer_id = new_member_id['id']
+                    # create data file object for the new member
+                    data_file = DataFile()
+                    data_file.member = instance
+                    data_file.file_upload_procedure = None
+                    data_file.data_file_path = None
+                    data_file.save()
+                    subscription_obj.save()
 
-    except ObjectDoesNotExist as ex:
-
-        return HttpResponseNotFound("Oops! Member Not found!")
+    except Exception as ex:
+        cprint(traceback.format_exc(), 'red')
+        log_exception(traceback.format_exc())
     else:
         if created:
-            print("members data file, usermembership are created successfully!")
+            cprint(f"subscription and data file object for member {instance.email} created successfully", 'green',
+                   'on_grey', attrs=['bold'])
 
 
-post_save.connect(create_usermembership_memberdatafile, sender=get_user_model())
+post_save.connect(create_subscription_datafile_object_for_member, sender=get_user_model())
