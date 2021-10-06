2d897395dc9e080449403bd3d69b38735c5e2f2a
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-10-06 21:38:29 +0300

    feat: rebuild the code and integrate it with backend and stripe features

--- predict_me/static/assets/js/custom/datahandler/data_handler_stripe.js
+++ predict_me/static/assets/js/custom/datahandler/data_handler_stripe.js
@@ -1,19 +1,66 @@
 "use strict";
-const oldStripeCardCheckbox = $("#oldStripeCardCheckbox")
+const dhUpgradeMembershipTableForm = document.querySelector("#dh-upgrade-membership-table-form");
+const dhBillingCycleInputs = document.querySelectorAll("[name='dh_upgrade_to_new_membership']");
+const oldStripeCardCheckbox = document.querySelector("#oldStripeCardCheckbox");
+const runStripeChargeBtn = document.querySelector("#runStripeChargeBtn");
+const dhExtraRecordsAllInputs = document.querySelectorAll(".data-handler-extra-records-form-input");
+
+dhBillingCycleInputs.forEach(item => {
+  item.addEventListener("blur", event => {
+    const currentElement = event.currentTarget;
+    const priceElement = currentElement.dataset['priceId'];
+    const priceElementNode = document.querySelector(`#${priceElement}`);
+    const placeholder = priceElementNode.querySelector("b.price-placeholder");
+    const monthly = priceElementNode.querySelector("b.price-monthly");
+    const yearly = priceElementNode.querySelector("b.price-yearly");
+    placeholder.classList.remove('d-none');
+    yearly.classList.add('d-none');
+    monthly.classList.add('d-none');
+    currentElement.checked = false;
+  });
+  item.addEventListener("change", event => {
+    const currentElement = event.currentTarget;
+    const priceElement = currentElement.dataset['priceId'];
+    const billingCycle = currentElement.dataset['cycle'];
+    const membershipName = currentElement.dataset['name'];
+    const newMembershipLbl = membershipName.replace("_", ' ').trim();
+    const priceElementNode = document.querySelector(`#${priceElement}`);
+    const placeholder = priceElementNode.querySelector("b.price-placeholder");
+    const monthly = priceElementNode.querySelector("b.price-monthly");
+    const yearly = priceElementNode.querySelector("b.price-yearly");
+
+    // console.log(newMembershipLbl);
+    createSessionStorage("newSubMembership", newMembershipLbl, true);
+
+    placeholder.classList.add('d-none'); // by default hide the placeholder price
+
+    // check if the user select monthly or yearly
+    if (billingCycle == "monthly") {
+      yearly.classList.add('d-none');
+      monthly.classList.remove('d-none');
+    } else if (billingCycle == "yearly") {
+      monthly.classList.add('d-none');
+      yearly.classList.remove('d-none');
+    }
+
+  });
+});
+
+// check if the button exists
+if (dhUpgradeMembershipTableForm !== null) {
+  dhUpgradeMembershipTableForm.addEventListener("submit", event => {
+    event.preventDefault();
+    $("#newCreditCardFormModal").modal("show");
+    $("#newCreditCardFormModal").modal("handleUpdate");
+  });
+}
+
+
 // Create a Stripe client.
-let stripe = Stripe("pk_test_g5z1xvZJJ04QG9uzwR9u0Df600SMC4ANGr");
+const stripe = Stripe('pk_test_g5z1xvZJJ04QG9uzwR9u0Df600SMC4ANGr', {
+  locale: 'en'
+});
 
-// hold var is for stripe input
-let stripeCardElementHolder = null;
-let stripeCardElementHolderClone = null;
-
-
-const extraRowsPurchased = $("#extraRowsPurchased");
-const totalAmountDue = $("#totalAmountDue");
-
-const dataHandlerSpinner = $("#data-handler-stripe-spinner");
-
-const dataHandlerStripeFormInputs = $("#data-handler-payment-form .data-handler-stripe-charge-form")
 // Create an instance of Elements.
 let elements = stripe.elements();
 
@@ -35,10 +82,14 @@ let style = {
   },
 };
 
-// Create an instance of the card Element.
-let card = elements.create("card", {
+// stripe card options
+const cardOptions = {
   style: style,
-});
+  hidePostalCode: true
+};
+
+// Create an instance of the card Element.
+let card = elements.create("card", cardOptions);
 
 // Add an instance of the card Element into the `card-element` <div>.
 card.mount("#card-element");
@@ -54,160 +105,166 @@ card.on("change", function (event) {
 });
 
 // Handle form submission.
-let form = document.getElementById("data-handler-payment-form");
+let form = document.querySelector("#data-handler-payment-form");
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  // first validate the extra records count
-  if ((extraRowsPurchased.val() === "") || (parseInt(extraRowsPurchased.val()) < 0)) {
-    extraRowsPurchased.addClass("border-danger");
-  } else {
-    // here if the extra records not empty
-    extraRowsPurchased.removeClass("border-danger");
-
-    if (oldStripeCardCheckbox.is(":checked") === true) {
-      // checked
-      showOrHideSpinner("SHOW");
-      stripeTokenHandler();
-    } else {
-      // not checked
+  try {
+    event.preventDefault();
+    const cardElement = elements.getElement("card");
+    if (cardElement !== null) {
       stripe.createToken(card).then(function (result) {
         if (result.error) {
           // Inform the user if there was an error.
-          let errorElement = document.getElementById("card-errors");
+          let errorElement = document.querySelector("#card-errors");
           errorElement.textContent = result.error.message;
         } else {
-          showOrHideSpinner("SHOW");
-          // Send the token to your server.
+
+          // disable the submit button
+          enableOrDisableNodeList(dhExtraRecordsAllInputs, "disable", disableAndWaitCursorCssClasses);
+          (cardElement !== null) && cardElement.update({
+            disabled: true
+          });
+
+          // Send the data to server.
           stripeTokenHandler(result.token);
+
+
         }
 
       });
+    } else {
+
+      // disable the submit button
+      enableOrDisableNodeList(dhExtraRecordsAllInputs, "disable", disableAndWaitCursorCssClasses);
+      (cardElement !== null) && cardElement.update({
+        disabled: true
+      });
+
+      // Send the data to server.
+      stripeTokenHandler();
 
     }
+  } catch (error) {
+    console.error(error);
   }
+
 
 
 });
 
 // Submit the form with the token ID.
-async function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  let stripeToken;
-  // check if the token exists
-  if (typeof token === "undefined") {
-    stripeToken = null;
-  } else {
-    stripeToken = token.id;
-  }
-  let form = document.getElementById("data-handler-payment-form");
-  let formData = {
-    "stripeToken": stripeToken,
-  };
-  const chargeRequest = await chargeExtraRecords(formData);
-  if (chargeRequest['is_done'] == true) {
-    Swal.fire(
-      'Success',
-      chargeRequest['msg'],
-      'success'
-    );
-    showOrHideSpinner("HIDE");
-    callDataHandlerTableFunc(); // update data handler overview
-  } else {
-    Swal.fire({
-      title: 'Attention!',
-      text: chargeRequest['msg'],
-      icon: 'error',
-      showCancelButton: false,
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Check',
-      allowEscapeKey: false,
-      allowEnterKey: false,
-      allowOutsideClick: false,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        showOrHideSpinner("HIDE");
-        // stripeCardElementHolder = null;
-      }
-    })
-
-  }
-
-}
-
-
-function enableDisableCheckBtn(isInReady, justAttach) {
-
-  const cEle = $("#card-element, #last4Span");
-  if (isInReady === true) {
-    oldStripeCardCheckbox.change(function () {
-      // console.log(stripeCardElementHolder);
-      if (this.checked) {
-        // if the checkbox is checked
-        stripeCardElementHolder = cEle.detach();
-        stripeCardElementHolderClone = $(stripeCardElementHolder).clone();
-      } else {
-        // if the checkbox not checked
-        // console.log(stripeCardElementHolder);
-
-        // console.log(stripeCardElementHolderClone.get(0) == stripeCardElementHolder.get(0));
-        if (stripeCardElementHolder !== null) {
-          $("#card-element-wrapper").append(stripeCardElementHolder);
-        }
-      }
-    });
-  } else {
-    // if isInReady is false
-    if (justAttach === true) {
-      // if (typeof stripeCardElementHolder[0] === "undefined"){
-      //   stripeCardElementHolder = stripeCardElementHolderClone;
-      // }
-      // console.log(stripeCardElementHolderClone[0]);
-      if (stripeCardElementHolder !== null) {
-        $("#card-element-wrapper").append(stripeCardElementHolder);
-      }
+const stripeTokenHandler = async (token) => {
+  try {
+    // Insert the token ID into the form so it gets submitted to the server
+    let stripeToken;
+    let formData = {};
+    // check if the token exists
+    if (typeof token === "undefined") {
+      stripeToken = null;
+      formData['is_new_card'] = false;
     } else {
-      stripeCardElementHolder = cEle.detach();
+      stripeToken = token.id;
+      formData['is_new_card'] = true;
+    }
+    formData['stripeToken'] = stripeToken;
+
+    // check if the member provide new credit card
+    if (formData['is_new_card'] === true) {
+      confirmRequestPromise().then(isConfirmed => {
+        // check if the user click on save or not
+        if (isConfirmed === true) {
+          formData['save_as_default'] = true;
+        } else {
+          formData['save_as_default'] = false;
+        }
+        return formData;
+
+      }).then(formData => {
+        const chargeRequest = chargeExtraRecords(formData);
+        chargeRequest.then(data => {
+          // check if there is an error in the request, the request for charge
+          if (data['is_error'] === false) {
+            // no errors in this case
+            displayTopNotification(data['msg'], "success");
+            setTimeout(() => {
+              location.reload();
+            }, 2000);
+
+          } else {
+            displayTopNotification(data['msg'], "danger");
+            throw new Error(data['msg']);
+          }
+        });
+      }).catch(error => {
+        displayTopNotification(error, "danger");
+      }).finally(() => {
+        setDataHandlerTableOverview();
+      });
+    } else {
+      // in case the member will charge using old credit card
+      const chargeRequest = chargeExtraRecords(formData);
+      chargeRequest.then(data => {
+        // check if there is an error in the request
+        if (data['is_error'] === false) {
+          // no errors in this case
+          displayTopNotification(data['msg'], "success");
+          const cardElement = elements.getElement("card");
+          enableOrDisableNodeList(dhExtraRecordsAllInputs, "enable", disableAndWaitCursorCssClasses);
+          (cardElement !== null) && cardElement.update({
+            disabled: true
+          });
+          setDataHandlerTableOverview();
+          setTimeout(() => {
+            location.reload();
+          }, 2000);
+
+
+        } else {
+          throw new Error(data['msg']);
+        }
+      }).catch(error => {
+        displayTopNotification(error, "danger");
+      }).finally(() => {
+        setDataHandlerTableOverview();
+      });
     }
 
 
-
+  } catch (error) {
+    console.error(error);
   }
+};
 
-
-}
-
-
-function showOrHideSpinner(showOrHide) {
-  if (showOrHide === "HIDE") {
-    // if set enable the
-    dataHandlerSpinner.hide();
-    dataHandlerStripeFormInputs.prop('disabled', false).removeClass('progress-cur');
-    enableDisableCheckBtn(false, true);
+oldStripeCardCheckbox.addEventListener("change", event => {
+  const element = event.currentTarget;
+  if (element.checked) {
+    card.destroy();
+    document.querySelector("#dh-card-element-wrapper").classList.add("d-none");
   } else {
-    dataHandlerSpinner.show();
-    dataHandlerStripeFormInputs.prop("disabled", true).addClass('progress-cur');
-    enableDisableCheckBtn(false, false);
+    document.querySelector("#dh-card-element-wrapper").classList.remove("d-none");
+    card = elements.create('card', cardOptions);
+    card.mount('#card-element');
+    // enable the submit button
+    runStripeChargeBtn.classList.remove("disabled", "wait-cursor");
+    runStripeChargeBtn.disabled = false;
   }
-}
-
-// this when the user enter the amount of rows which will purchased
-function calculateExtraRows() {
-
-  // check if the number of rows bigger than 0
-  if (parseFloat(extraRowsPurchased.val()) < 0) {
-    totalAmountDue.val(0);
-  } else {
-    totalAmountDue.val(parseFloat(extraRowsPurchased.val() * 0.5));
-  }
-
-}
-
-
-
-$(function () {
-  // code
-  enableDisableCheckBtn(true);
-  // console.log($("#dhAbovePlanLimitRecord").text())
-
-  
 });
+
+const confirmRequestPromise = () => {
+  return new Promise((resolve, reject) => {
+    Swal.fire({
+      title: 'Attention',
+      html: "Do you want to save the new credit card as the default on <b>PredictME</b> project?",
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, save it!',
+      cancelButtonText: "No, just use it for this charge",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+    }).then((result) => {
+      resolve(result.isConfirmed);
+    });
+  });
+}
