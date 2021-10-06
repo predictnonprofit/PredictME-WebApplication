ccde607b8d77511eb511a5278ac527e992aec4c1
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-10-06 21:36:34 +0300

    fix: fix change-stripe-card url

--- membership/urls.py
+++ membership/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # check where this used in template files don't delete the class view
     # path("upgrade-membership", UpgradeMembershipView.as_view(), name='membership-upgrade-membership'),
     path("charge-extra-records", ChargeExtraRecordView.as_view(), name='membership-charge-extra-records'),
-    path("change-stripe-card", ChangeStripeCardView.as_view(), name='membership-change-stripe-card'),
+    path("upgrade-stripe-subscription", UpgradeStripeSubscriptionView.as_view(),
+         name='membership-upgrade-stripe-subscription'),
     path("api/", include([
         path("upgrade-membership-api-only", UpgradeMembershipAPIView.as_view(), name='api-upgrade-membership-only')
     ]), name='membership-api-urls')
