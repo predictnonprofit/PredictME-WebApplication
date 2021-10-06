257c83b8548d2264cd6a42012fe3e6f6f6322de5
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-10-06 21:39:12 +0300

    fix: fix variable scope

--- predict_me/static/assets/js/custom/members_app/stripe-update.js
+++ predict_me/static/assets/js/custom/members_app/stripe-update.js
@@ -1,15 +1,16 @@
+"use strict";
 $(function () {
     // Create a Stripe client.
-    var stripe = Stripe('pk_test_g5z1xvZJJ04QG9uzwR9u0Df600SMC4ANGr', {
+    const stripe = Stripe('pk_test_g5z1xvZJJ04QG9uzwR9u0Df600SMC4ANGr', {
         locale: 'en'
     });
 
     // Create an instance of Elements.
-    var elements = stripe.elements();
+    const elements = stripe.elements();
 
     // Custom styling can be passed to options when creating an Element.
     // (Note that this demo uses a wider set of styles than the guide below.)
-    var style = {
+    const style = {
         base: {
             color: '#32325d',
             fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -26,8 +27,9 @@ $(function () {
     };
 
     // Create an instance of the card Element.
-    var card = elements.create('card', {
-        style: style
+    const card = elements.create('card', {
+        style: style,
+        hidePostalCode: true
     });
 
     // Add an instance of the card Element into the `card-element` <div>.
@@ -35,7 +37,7 @@ $(function () {
 
     // Handle real-time validation errors from the card Element.
     card.addEventListener('change', function (event) {
-        var displayError = document.getElementById('card-errors');
+        const displayError = document.querySelector('#card-errors');
         if (event.error) {
             displayError.textContent = event.error.message;
         } else {
@@ -44,20 +46,22 @@ $(function () {
     });
 
     // Handle form submission.
-    var form = document.getElementById('update-credit-card-form');
+    const form = document.querySelector('#update-credit-card-form');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         const updateBtn = document.querySelector("#memberUpdateCreditCardBtn");
-        updateBtn.classList.add("disabled", "wait-cursor");
-        updateBtn.disabled = true;
+
         stripe.createToken(card).then(function (result) {
             if (result.error) {
                 // Inform the user if there was an error.
-                var errorElement = document.getElementById('card-errors');
+                const errorElement = document.getElementById('card-errors');
                 errorElement.textContent = result.error.message;
             } else {
                 // Send the token to your server.
                 stripeTokenHandler(result.token);
+                // disable submit button
+                updateBtn.classList.add("disabled", "wait-cursor");
+                updateBtn.disabled = true;
             }
         });
     });
@@ -65,8 +69,8 @@ $(function () {
     // Submit the form with the token ID.
     function stripeTokenHandler(token) {
         // Insert the token ID into the form so it gets submitted to the server
-        var form = document.getElementById('update-credit-card-form');
-        var hiddenInput = document.createElement('input');
+        const form = document.querySelector('#update-credit-card-form');
+        const hiddenInput = document.createElement('input');
         hiddenInput.setAttribute('type', 'hidden');
         hiddenInput.setAttribute('name', 'stripeToken');
         hiddenInput.setAttribute('value', token.id);
