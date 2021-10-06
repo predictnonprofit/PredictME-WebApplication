85f28b1ba44fc00111731ee5f56a72c7cea6f4b2
Ibrahim Luqman <ibm_luq95@yahoo.com>
2021-10-06 21:36:49 +0300

    feat: add new tag create_dict_from_string

--- predict_me/templatetags/general_tags.py
+++ predict_me/templatetags/general_tags.py
@@ -1,3 +1,4 @@
+import ast
 from django import template
 
 register = template.Library()
@@ -6,3 +7,8 @@ register = template.Library()
 @register.filter
 def get_type(value):
     return type(value).__name__.strip()
+
+
+@register.simple_tag
+def create_dict_from_string(txt):
+    return ast.literal_eval(txt)
